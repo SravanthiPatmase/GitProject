c70215ba2af691d88f717be99f1ccbb09186fc3a
Sravanthi Patmase <sravanthi.patmase@smscountry.com>
2019-08-21 15:54:05 +0530

    Added j var

--- src/Demo1.java
+++ src/Demo1.java
@@ -3,12 +3,6 @@ public class Demo1
 {
   public static void main(String args[])
   {
-	 /* String s="hai";
-	  if(s=="hai")
-		  System.out.println("Hai");
-	  else
-		  System.out.println("Bye");*/
-	  
 	  int j=10;
 	  for(j=1;j<=10;j++)
 	  {
