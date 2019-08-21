53d19a2653e18277bfee2f36e6bdb4ef315674e7
Sravanthi Patmase <sravanthi.patmase@smscountry.com>
2019-08-21 15:14:51 +0530

    First Commit

---
+++ src/Demo1.java
@@ -0,0 +1,12 @@
+
+public class Demo1
+{
+  public static void main(String args[])
+  {
+	  String s="hai";
+	  if(s=="hai")
+		  System.out.println("Hai");
+	  else
+		  System.out.println("Bye");
+  }
+}
