4492197af82a7de1029ae1feaeebcc6f9664efa5
Sravanthi Patmase <sravanthi.patmase@smscountry.com>
2019-08-21 15:28:53 +0530

    Added for-loop to print 10 integers

--- src/Demo1.java
+++ src/Demo1.java
@@ -3,10 +3,18 @@ public class Demo1
 {
   public static void main(String args[])
   {
-	  String s="hai";
+	 /* String s="hai";
 	  if(s=="hai")
 		  System.out.println("Hai");
 	  else
-		  System.out.println("Bye");
+		  System.out.println("Bye");*/
+	  
+	  
+	  
+	  int i=10;
+	  for(i=1;i<=10;i++)
+	  {
+		  System.out.println(i);
+	  }
   }
 }
